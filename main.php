a395cab536180eeacb82157ecafff0eab02502e4
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2023-10-02 09:02:17 +0200

    working with styling

--- main.php
+++ main.php
@@ -6,15 +6,26 @@ require __DIR__ . "/data.php";
 
 <main>
     <table>
-        <?
-        foreach ($teams as $key => $team) : ?>
-            <th><?= $key; ?></th>
-        <?
-        endforeach;
-        ?>
         <tr>
-            <td>Test</td>
+            <!--loops through teams-array and finds the names of all the teams.-->
+            <?
+            foreach ($teams as $key => $team) : ?>
+
+
+                <th><?= $key; ?></th>
+
+        <tr>
+            <td><?= $team['league']; ?></td>
+            <td><?= $team['last-time-champions']; ?></td>
+            <td><?= $team['city']; ?></td>
+            <td><?= $team['url']; ?></td>
         </tr>
+
+    <?
+            endforeach;
+    ?>
+    </tr>
+
     </table>
     <table>
         <tr>
