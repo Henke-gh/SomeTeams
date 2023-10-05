22d600eead98bafbacfe4b7dd40245cd543f3a3d
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2023-10-05 15:01:30 +0200

    added style-tag to index to solve css-issue

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Some Teams - WU23</title>
-    <link rel="stylesheet" href="/customers/3/4/5/henkes-portfolio.se/httpd.www/SomeTeams" />
+    <!--<link rel="stylesheet" href="/css/styles.css" />-->
 
 </head>
 
