77ae372290aafe394b9a8149034704ad64a9ffe9
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2023-10-05 15:05:50 +0200

    stylesheet in header finally solved.

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<!--Feels wrong to solve the css like this but couldn't find another way.
-Linking the stylesheet from within the HTML-head-tag didn't work once
-uploaded to one.com -->
-<style>
-    <?php
-    include 'css/styles.css';
-    ?>
-</style>
 <?
 
 require __DIR__ . "/head.php";
